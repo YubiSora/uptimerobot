1bafe3bb393b2c6834a124fdc6c9e1cce72cbbef
YubiSora <94666378+YubiSora@users.noreply.github.com>
2022-01-27 13:43:41 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const axios = require("axios")
-const urls = ["https://spiral-treasure-rhubarb.glitch.me"]
+const urls = ["https://humdrum-rounded-azimuth.glitch.me"]
 setInterval(function() {
             urls.forEach(url => {
             axios.get(url).then(console.log("Pong at " + Date.now())).catch(() => {});
